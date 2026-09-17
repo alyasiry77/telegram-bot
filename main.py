3cae35d712728b5a4c7f08bf5215703c8228711a
alyasiry77 <alysiry2022@gmail.com>
2026-09-18 01:05:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import asyncio
+
+# إضافة مسار المجلد الحالي ليقرأ كل الملفات والمجلدات بداخله تلقائياً
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from bot import main
 
 if __name__ == "__main__":
