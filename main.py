fff98b522bba28109ccf3ac9d7b6efdca5bf514a
alyasiry77 <alysiry2022@gmail.com>
2026-09-18 01:35:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 import os
+
+# إضافة مجلد bot إلى مسار البحث الخاص ببايثون
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'bot')))
+
 import asyncio
-
-# إضافة مسار المجلد الحالي ليقرأ كل الملفات والمجلدات بداخله تلقائياً
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-from bot import main
+from bot.bot import main as bot_main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(bot_main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Bot stopped!")
